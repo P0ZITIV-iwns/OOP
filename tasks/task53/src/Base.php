38ebf349b1e692bc42ff94a5395c5ab3fc12ca6e
P0ZITIV <66378765+P0ZITIV-iwns@users.noreply.github.com>
2023-11-24 23:24:20 +0500

    update

--- tasks/task53/src/Base.php
+++ tasks/task53/src/Base.php
@@ -6,17 +6,6 @@ class Base
 {
     public function isInstanceOf($className)
     {
-        $currentClass = get_class($this);
-
-        if ($currentClass === $className) {
-            return true;
-        }
-
-        $parents = class_parents($this);
-
-        if (in_array($className, $parents)) {
-            return true;
-        }
-        return false;
+        return (get_class($this) === $className || in_array($className, class_parents($this)));
     }
 }
